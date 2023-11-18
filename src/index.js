7a4a5735b2c10bf2fbb9231ab92a9759c781db11
Balaji Rajagopalan <balajiraja1986@gmail.com>
2023-11-18 13:08:54 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./styles/appstyle.css";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>
